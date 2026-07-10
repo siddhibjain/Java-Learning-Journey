4d8236eb396221528f80ffbd0a2a12091368caf4
Siddhi Banthia Jain <siddhibanthiajain@gmail.com>
2026-07-10 22:34:36 +0530

    Add Day7:Arrays

---
+++ Day07-Array/SwapFirstAndLast.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class SwapFirstAndLast
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of  array:");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        System.out.println("Enter  array");
+        for (int i = 0; i < size; i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Before swapping first and last element:");
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+       for(int i=0;i<arr.length;i++)
+       {
+           int temp=arr[0];
+           arr[0]=arr[arr.length-1];
+           arr[arr.length-1]=temp;
+       }
+       System.out.println("After swapping first and last element:");
+       for(int i=0;i<arr.length;i++)
+       {
+           System.out.print(arr[i]+" ");
+       }
+    }
+}
